ead1a5a140c8378c1da3a8099f765cfa18019de3
theraggedflygon <68510342+theraggedflygon@users.noreply.github.com>
2025-09-02 15:05:35 -0500

    Adds status indicator for trials in modal

--- src/app/components/progressModal.tsx
+++ src/app/components/progressModal.tsx
@@ -1,8 +1,44 @@
-import { useTrials } from "@/context/trialContext";
+import { ITrial, TrialStatus, useTrials } from "@/context/trialContext";
 import * as React from "react";
 
 const ProgressModal = ({ isOpen, closeModal }: ProgressModalProps) => {
   const { trials } = useTrials();
+
+  const completedTrials = trials
+    .map((trial): number => (trial.complete ? 1 : 0))
+    .reduce((sum, val) => val + sum, 0);
+
+  const renderStatusIcon = (trial: ITrial) => {
+    if (trial.complete === TrialStatus.COMPLETE) {
+      return (
+        <div className="bg-green-500 w-4 h-4 rounded-[50%] mt-0.75 mx-1 flex justify-center items-center text-white leading-4">
+          ✓
+        </div>
+      );
+    } else if (trial.complete === TrialStatus.PASSED) {
+      return (
+        <div className="bg-gray-500 w-4 h-4 rounded-[50%] mt-0.75 mx-1 flex justify-center items-center text-white leading-4">
+          ✓
+        </div>
+      );
+    } else if (
+      trial.years
+        .map((year): number => (year.institutionIds.length > 0 ? 1 : 0))
+        .reduce((acc, val) => acc + val, 0) !== 0
+    ) {
+      return (
+        <div className="bg-yellow-400 w-4 h-4 rounded-[50%] mt-0.75 mx-1 flex justify-center items-center text-white">
+          ⏱
+        </div>
+      );
+    } else {
+      return (
+        <div className="bg-red-500 w-4 h-4 rounded-[50%] mt-0.75 mx-1 flex justify-center items-center text-white">
+          ×
+        </div>
+      );
+    }
+  };
 
   if (!isOpen) {
     return <div></div>;
@@ -14,11 +50,15 @@ const ProgressModal = ({ isOpen, closeModal }: ProgressModalProps) => {
         className="bg-gray-500 opacity-50 z-10 fixed w-full h-full left-0 top-0"
         onClick={closeModal}
       ></div>
-      <div className="bg-gray-200 z-20 fixed left-1/4 w-1/2 rounded-lg p-5">
+      <div className="bg-gray-200 z-20 absolute left-1/4 top-5 w-1/2 rounded-lg p-5">
         <div className="flex flex-row justify-between w-full">
-          <div>Trial Progress Tracker</div>
+          <div>
+            Trial Progress Tracker - Overall Progress: {completedTrials} out of{" "}
+            {trials.length} (
+            {Math.round((completedTrials * 100) / trials.length)}%)
+          </div>
           <button
-            className="bg-red-300 px-1.5 rounded-sm hover:cursor-pointer hover:bg-red-500"
+            className="bg-red-300 px-2.5 py-1 rounded-sm hover:cursor-pointer hover:bg-red-500"
             onClick={closeModal}
           >
             ×
@@ -26,8 +66,12 @@ const ProgressModal = ({ isOpen, closeModal }: ProgressModalProps) => {
         </div>
         <div className="px-5">
           {trials.map((trial, idx) => (
-            <div key={trial.nctID}>
-              {idx + 1}. {trial.nctID} - {trial.complete}
+            <div key={trial.nctID} className="flex flex-row">
+              {idx + 1}. {trial.nctID} {renderStatusIcon(trial)}
+              {trial.years
+                .map((year): number => (year.institutionIds.length > 0 ? 1 : 0))
+                .reduce((acc, val) => acc + val, 0)}{" "}
+              out of {trial.years.length}
             </div>
           ))}
         </div>
